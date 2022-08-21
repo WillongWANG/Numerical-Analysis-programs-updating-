ced02ae99358f9e52f198a97f8ea9faab3991be8
xiaoweiweiwei1 <107450308+xiaoweiweiwei1@users.noreply.github.com>
2022-08-21 10:00:52 +0800

    Add files via upload

---
+++ lagrange.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#define n 3                         // Lagrange,数字为输入点个数
+main()                               
+{
+    float x, x_[n], y_[n]; int k, j; float t, y;
+    printf("\nPlease enter x_: ");     
+    for(k=0;k<=n-1;k++)              
+    scanf_s("%f",&x_[k]);
+        printf("\nPlease enter y_:");
+        for (k = 0; k <= n-1; k++)
+            scanf_s("%f", &y_[k]);
+          printf("\nPlease enter x");
+          scanf_s("%f", &x);
+        for (k = 0,y=0; k <= n-1; k++)
+        {
+            for (j = 0,t=1; j <= n-1; j++)
+            {
+                if (j == k) continue;
+                t = t * (x - x_[j]) / (x_[k] - x_[j]); 
+            y = y + t * y_[k];
+        }    
+    printf("x为%f时，y为%f",x,y);
+} 
